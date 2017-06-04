4e56917b4bcc884ed2c7a03696877bcc33174a23
pikaqqqqq <281705236@qq.com>
2017-06-04 19:34:33 +0800

    0.2使用匿名的时机

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by think on 2017/6/4.
@@ -11,6 +13,14 @@ public class TankClient extends Frame{
     public void launch(){
         setLocation(200,100);
         setSize(800,600);
+        setTitle("TankWar");
+        setResizable(false);//不让窗口改变大小
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
         setVisible(true);
     }
 }
