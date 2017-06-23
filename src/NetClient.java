9b91505435a6e6e3ba605e07d75cf4912102f647
pikaqqqqq <281705236@qq.com>
2017-06-23 21:59:09 +0800

    //1.9.3_1保存udp端口

--- src/NetClient.java
+++ src/NetClient.java
@@ -1,3 +1,4 @@
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -6,17 +7,33 @@ import java.net.Socket;
  */
 public class NetClient {
 
-    public static final int UDP_PORT = 5566;
+    /**UDP端口如何定义（冲突问题）
+     *
+     */
+    private static int UDP_PORT_START = 2223;
+    private int udpPort;
 
     private TankClient tc;
     private Socket s = null;
 
+    public NetClient() {
+        this.udpPort = UDP_PORT_START++;
+    }
+
     public void connect(String IP, int port){
         try {
-            Socket s = new Socket(IP, port);
+           s = new Socket(IP, port);
 System.out.println("connect to server!");
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeInt(udpPort);
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            try {
+                if (s != null) s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
--- src/TankServer.java
+++ src/TankServer.java
@@ -1,6 +1,9 @@
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by think on 2017/6/23.
@@ -12,14 +15,21 @@ public class TankServer {
      */
 
     public static final int TCP_PORT = 8888;
+    List<Client> clients = new ArrayList<Client>();
 
-    public void launch(){
+    public void start(){
         ServerSocket ss = null;
         try {
             ss = new ServerSocket(TCP_PORT);
             while (true){
                 Socket s = ss.accept();
-System.out.println("a client connect! Addr:" + s.getInetAddress() + ":" + s.getPort() );
+System.out.println("a client connect! Addr:" + s.getInetAddress().getHostAddress() + ":" + s.getPort() );
+                String IP = s.getInetAddress().getHostAddress();
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                int udpPort = dis.readInt();
+                Client client = new Client(IP, udpPort);
+                clients.add(client);
+                s.close();//握手的机制
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -27,8 +37,17 @@ System.out.println("a client connect! Addr:" + s.getInetAddress() + ":" + s.getP
 
     }
 
+    private class Client{
+        String IP;
+        int udpPort;
+
+        public Client(String IP, int udpPort) {
+            this.IP = IP;
+            this.udpPort = udpPort;
+        }
+    }
 
     public static void main(String[] args){
-        new TankServer().launch();
+        new TankServer().start();
     }
 }
